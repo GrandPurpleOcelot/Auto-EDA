d5f28820c5e17b69b638346589bbabc199e2a168
Thien Nghiem <54216383+GrandPurpleOcelot@users.noreply.github.com>
2020-07-17 18:18:16 -0600

    updated build

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="majora", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Thien Nghiem",
     author_email="thien.nghiem94@gmail.com",
     description="Majora is a python library that automates common tasks in your exploratory data analysis. ",
@@ -21,7 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/GrandPurpleOcelot/Auto-EDA",
     packages=setuptools.find_packages(),
-    install_requires=install_requires
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
